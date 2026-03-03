fcd9ce5ca31f385cb5d70de77e7b9dc34ffe7b62
Jmee66 <Jmee66@users.noreply.github.com>
2026-03-03 21:18:24 +0000

    deploy: e00891c866a6ceddb76c4f612df1da8f9d617f89

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/fishing-weather-app/sw.js', { scope: '/fishing-weather-app/' })})}
